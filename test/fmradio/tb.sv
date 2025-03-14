1c3d226dfa327bccc3573c7edfab74109fe65f5d
Liam Patterson <liampatterson@u.northwestern.edu>
2025-03-14 11:39:48 -0500

    fm_radio tb updates

--- test/fmradio/tb.sv
+++ test/fmradio/tb.sv
@@ -4,10 +4,10 @@
 module fm_radio_tb;
 
 localparam string A_IN  = "../a.bin";
-localparam string CMP_IN = "../cmpL.bin";
-localparam string CMP_IN2 = "../cmpR.bin";
-localparam string OUT_NAME = "../outL.bin";
-localparam string OUT_NAME2 = "../outR.bin";
+localparam string CMP_IN = "../cmpL.txt";
+localparam string CMP_IN2 = "../cmpR.txt";
+localparam string OUT_NAME = "../outL.txt";
+localparam string OUT_NAME2 = "../outR.txt";
 
 localparam CLOCK_PERIOD = 10;
 
@@ -23,48 +23,34 @@ logic   hold_clock    = '0;
 logic   in_write_done = '0;
 logic   out_read_done = '0;
 integer out_errors    = '0;
-/*module fm_radio (
-    input  logic         clock,
-    input  logic         reset,
 
-    input logic         iq_sample_wr_en,      
-    input  logic [31:0]        iq_sample,      
-    output logic          iq_sample_full,
 
-    input  logic         left_audio_out_rd_en,    
-    output logic          left_audio_out_empty,   
-    output logic signed [31:0] left_audio_out,
-    
-    input  logic         right_audio_out_rd_en,
-    output  logic         right_audio_out_empty,
-    output logic signed [31:0] right_audio_out
-);*/
 
-logic fm_radio_in_wr_en = '0;
-logic [31:0] fm_radio_in_din = '0;
-logic fm_radio_in_full;
+logic iq_sample_wr_en = '0;
+logic [31:0] iq_sample = 31'b0;
+logic iq_sample_full;
 
-logic fm_radio_out_left_rd_en = '0;
-logic fm_radio_out_left_empty;
-logic signed [31:0] fm_radio_out_left;
+logic left_audio_out_rd_en = '0;
+logic right_audio_out_rd_en = '0;
+logic left_audio_out_empty;
+logic right_audio_out_empty;
+logic signed [31:0] left_audio_out;
+logic signed [31:0] right_audio_out;
 
-logic fm_radio_out_right_rd_en = '0;
-logic fm_radio_out_right_empty;
-logic signed [31:0] fm_radio_out_right;
-
-fm_radio fm_radio_inst (
+fm_radio read_iq_inst (
     .clock(clock),
     .reset(reset),
-    .iq_sample_wr_en(fm_radio_in_wr_en),
-    .iq_sample(fm_radio_in_din),
-    .iq_sample_full(fm_radio_in_full),
-    .left_audio_out_rd_en(fm_radio_out_left_rd_en),
-    .left_audio_out_empty(fm_radio_out_left_empty),
-    .left_audio_out(fm_radio_out_left),
-    .right_audio_out_rd_en(fm_radio_out_right_rd_en),
-    .right_audio_out_empty(fm_radio_out_right_empty),
-    .right_audio_out(fm_radio_out_right)
+    .iq_sample_wr_en(iq_sample_wr_en),
+    .iq_sample(iq_sample),
+    .iq_sample_full(iq_sample_full),
+    .left_audio_out_rd_en(left_audio_out_rd_en),
+    .left_audio_out_empty(left_audio_out_empty),
+    .left_audio_out(left_audio_out),
+    .right_audio_out_rd_en(right_audio_out_rd_en),
+    .right_audio_out_empty(right_audio_out_empty),
+    .right_audio_out(right_audio_out)
 );
+
 
 always begin
     clock = 1'b1;
@@ -111,7 +97,7 @@ initial begin : img_read_process
 
     @(negedge reset);
     $display("@ %0t: Loading in file %s...", $time, A_IN);
-    a_file = $fopen(A_IN, "r");
+    a_file = $fopen(A_IN, "rb");
     if (a_file == 0) begin
         $display("Error: Could not open file %s", A_IN);
         $finish;
@@ -122,16 +108,16 @@ initial begin : img_read_process
     i = 0;
     while (!$feof(a_file)) begin
         @(negedge clock);
-        fm_radio_in_wr_en = 1'b0;
-        if(!fm_radio_in_full) begin
-            r = $fread(fm_radio_in_din, a_file, i*4, 4);
-            fm_radio_in_wr_en = 1'b1;
+        iq_sample_wr_en = 1'b0;
+        if(!iq_sample_full) begin
+            r = $fread(iq_sample, a_file, i*4, 4);
+            iq_sample_wr_en = 1'b1;
             i++;
         end
     end
 
     @(negedge clock);
-    fm_radio_in_wr_en = 1'b0;
+    iq_sample_wr_en = 1'b0;
     $fclose(a_file);
     in_write_done = 1'b1;
 end
@@ -156,24 +142,24 @@ initial begin : img_write_process
     out_file2 = $fopen(OUT_NAME2, "w");
     cmp_file = $fopen(CMP_IN, "r");
     cmp_file2 = $fopen(CMP_IN2, "r");
-    fm_radio_out_left_rd_en = 1'b0;
-    fm_radio_out_right_rd_en = 1'b0;
+    left_audio_out_rd_en = 1'b0;
+    right_audio_out_rd_en = 1'b0;
 
     i = 0;
-    while (!$feof(cmp_file) && !$feof(cmp_file2)) begin
+    while (!$feof(cmp_file)) begin
         @(negedge clock);
-        fm_radio_out_left_rd_en = 1'b0;
-        fm_radio_out_right_rd_en = 1'b0;
+        left_audio_out_rd_en = 1'b0;
+        right_audio_out_rd_en = 1'b0;
         // Read from the sum FIFO if it's not empty
-        if (fm_radio_out_left_empty == 1'b0 && fm_radio_out_right_empty == 1'b0) begin
-            fm_radio_out_left_rd_en = 1'b1;
-            fm_radio_out_right_rd_en = 1'b1;
-            r = $fread(cmp_value, cmp_file, i*4, 4);
-            r = $fread(cmp_value2, cmp_file2, i*4, 4);
-            sum_value2 = fm_radio_out_right;
-            sum_value = fm_radio_out_left;
-            $fwrite(out_file, "%c", sum_value); 
-            $fwrite(out_file2, "%c", sum_value2);
+        if (left_audio_out_empty == 1'b0 && right_audio_out_empty == 1'b0) begin
+            left_audio_out_rd_en = 1'b1;
+            right_audio_out_rd_en = 1'b1;
+            r = $fscanf(cmp_file, "%d\n", cmp_value);
+            r = $fscanf(cmp_file2, "%d\n", cmp_value2);
+            sum_value2 = right_audio_out;
+            sum_value = left_audio_out;
+            $fwrite(out_file, "%d\n", sum_value); 
+            $fwrite(out_file2, "%d\n", sum_value2);
             if (sum_value != cmp_value) begin
                 out_errors += 1;
                 $display("@ %0t: ERROR: Mismatch at line %0d: Expected %d, Got %d", $time, i+1, cmp_value, sum_value);
@@ -185,8 +171,8 @@ initial begin : img_write_process
             i += 1;
         end
     end
-    fm_radio_out_left_rd_en = 1'b0;
-    fm_radio_out_right_rd_en = 1'b0;
+    left_audio_out_rd_en = 1'b0;
+    right_audio_out_rd_en = 1'b0;
     @(negedge clock);
     $fclose(out_file);
     $fclose(cmp_file);
